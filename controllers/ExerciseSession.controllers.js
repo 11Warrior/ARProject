fca199d49eeab0ea7a276b04ffa6b54ba4bda649
Aayush <aayushthapa640@gmail.com>
2025-05-15 21:10:44 +0530

    final0.0

--- controllers/ExerciseSession.controllers.js
+++ controllers/ExerciseSession.controllers.js
@@ -1,40 +1,5 @@
 // const { ReturnDocument } = require("mongodb");
-const { ExerciseSession } = require("../models/exerciseSession.models");
 
-const startSession = async (req, res, next) => {
-    if(!req.body.workoutType){
-        return res.status(400).json({
-            message:'Invalid Request'
-        })
-    }
-    var exerciseSession = await ExerciseSession.startExcerciseSession({
-        workoutType: req.body.workoutType
-    })
-    return res.status(200).json({
-        sessionID: exerciseSession._id
-    })
-}
-
-const endSession = async (req, res, next) => {
-    if(!req.body.sessionID || !req.body.accuracy){
-        return res.status(400).json({
-            message:'Invalid Request'
-        })
-    }
-    await ExerciseSession.endExerciseSession({
-        sessionID: req.body.sessionID,
-        accuracy: req.body.accuracy
-    })
-    return res.status(200).json({
-        sessionID: req.body.sessionID,
-        message:'session ended'
-    })
-}
-
-const getSessions = async (req, res, next) => {
-    var sessions = await ExerciseSession.getUserSessions()
-    return res.status(200).json(sessions)
-}
 
 const workout = async (req, res, next) => {
     const workoutType = req.query.type || 'bicep';
@@ -42,8 +7,6 @@ const workout = async (req, res, next) => {
 }
 
 module.exports = {
-    startSession,
-    endSession,
-    getSessions,
+
     workout
 };
