ace458161615889e04323e0ae28115fd084fcfb2
Aayush <aayushthapa640@gmail.com>
2025-05-16 17:09:13 +0530

    final1.0

--- public/js/Sketcher.js
+++ public/js/Sketcher.js
@@ -25,6 +25,10 @@ class Sketcher{
                 this.detectBicepCurl(keypoints);
             } else if (this.workoutType === 'situp') {
                 this.detectSitup(keypoints);
+            } else if (this.workoutType === 'squats') {
+                this.detectSquat(keypoints);
+            } else if (this.workoutType === 'shoulderPress') {
+                this.detectShoulderPress(keypoints);
             }
         }
     }
@@ -140,6 +144,151 @@ class Sketcher{
                 this.wasDown = false; // Prevent double count
             }
             if (isSitupDown) {
+                this.wasDown = true;
+            }
+        }
+    }
+
+    detectSquat(keypoints) {
+        const leftHip = keypoints.find(
+            kp => kp.name === "left_hip" || kp.part === "leftHip"
+        );
+        const rightHip = keypoints.find(
+            kp => kp.name === "right_hip" || kp.part === "rightHip"
+        );
+        const leftKnee = keypoints.find(
+            kp => kp.name === "left_knee" || kp.part === "leftKnee"
+        );
+        const rightKnee = keypoints.find(
+            kp => kp.name === "right_knee" || kp.part === "rightKnee"
+        );
+        const leftAnkle = keypoints.find(
+            kp => kp.name === "left_ankle" || kp.part === "leftAnkle"
+        );
+        const rightAnkle = keypoints.find(
+            kp => kp.name === "right_ankle" || kp.part === "rightAnkle"
+        );
+
+        if (
+            leftHip && rightHip && leftKnee && rightKnee &&
+            leftAnkle && rightAnkle &&
+            (leftHip.score === undefined || leftHip.score > 0.4) &&
+            (rightHip.score === undefined || rightHip.score > 0.4) &&
+            (leftKnee.score === undefined || leftKnee.score > 0.4) &&
+            (rightKnee.score === undefined || rightKnee.score > 0.4) &&
+            (leftAnkle.score === undefined || leftAnkle.score > 0.4) &&
+            (rightAnkle.score === undefined || rightAnkle.score > 0.4)
+        ) {
+            // Calculate average positions
+            const avgHipY = (leftHip.y + rightHip.y) / 2;
+            const avgKneeY = (leftKnee.y + rightKnee.y) / 2;
+            const avgAnkleY = (leftAnkle.y + rightAnkle.y) / 2;
+
+            // Calculate knee angles
+            const leftKneeAngle = Math.abs(Math.atan2(
+                leftHip.y - leftKnee.y,
+                leftHip.x - leftKnee.x
+            ) - Math.atan2(
+                leftAnkle.y - leftKnee.y,
+                leftAnkle.x - leftKnee.x
+            )) * 180 / Math.PI;
+
+            const rightKneeAngle = Math.abs(Math.atan2(
+                rightHip.y - rightKnee.y,
+                rightHip.x - rightKnee.x
+            ) - Math.atan2(
+                rightAnkle.y - rightKnee.y,
+                rightAnkle.x - rightKnee.x
+            )) * 180 / Math.PI;
+
+            const avgKneeAngle = (leftKneeAngle + rightKneeAngle) / 2;
+
+            // Squat is considered "down" when knees are bent significantly
+            const isSquatDown = avgKneeAngle < 100;
+            // Squat is considered "up" when knees are nearly straight
+            const isSquatUp = avgKneeAngle > 160;
+
+            // Initialize wasDown if not present
+            if (this.wasDown === undefined) this.wasDown = false;
+
+            // Only count when going from down to up
+            if (isSquatUp && this.wasDown) {
+                if (window.incrementReps) window.incrementReps();
+                this.wasDown = false;
+            }
+            if (isSquatDown) {
+                this.wasDown = true;
+            }
+        }
+    }
+
+    detectShoulderPress(keypoints) {
+        const leftShoulder = keypoints.find(
+            kp => kp.name === "left_shoulder" || kp.part === "leftShoulder"
+        );
+        const rightShoulder = keypoints.find(
+            kp => kp.name === "right_shoulder" || kp.part === "rightShoulder"
+        );
+        const leftElbow = keypoints.find(
+            kp => kp.name === "left_elbow" || kp.part === "leftElbow"
+        );
+        const rightElbow = keypoints.find(
+            kp => kp.name === "right_elbow" || kp.part === "rightElbow"
+        );
+        const leftWrist = keypoints.find(
+            kp => kp.name === "left_wrist" || kp.part === "leftWrist"
+        );
+        const rightWrist = keypoints.find(
+            kp => kp.name === "right_wrist" || kp.part === "rightWrist"
+        );
+
+        if (
+            leftShoulder && rightShoulder && leftElbow && rightElbow &&
+            leftWrist && rightWrist &&
+            (leftShoulder.score === undefined || leftShoulder.score > 0.4) &&
+            (rightShoulder.score === undefined || rightShoulder.score > 0.4) &&
+            (leftElbow.score === undefined || leftElbow.score > 0.4) &&
+            (rightElbow.score === undefined || rightElbow.score > 0.4) &&
+            (leftWrist.score === undefined || leftWrist.score > 0.4) &&
+            (rightWrist.score === undefined || rightWrist.score > 0.4)
+        ) {
+            // Calculate average positions
+            const avgShoulderY = (leftShoulder.y + rightShoulder.y) / 2;
+            const avgWristY = (leftWrist.y + rightWrist.y) / 2;
+
+            // Calculate arm angles
+            const leftArmAngle = Math.abs(Math.atan2(
+                leftShoulder.y - leftElbow.y,
+                leftShoulder.x - leftElbow.x
+            ) - Math.atan2(
+                leftWrist.y - leftElbow.y,
+                leftWrist.x - leftElbow.x
+            )) * 180 / Math.PI;
+
+            const rightArmAngle = Math.abs(Math.atan2(
+                rightShoulder.y - rightElbow.y,
+                rightShoulder.x - rightElbow.x
+            ) - Math.atan2(
+                rightWrist.y - rightElbow.y,
+                rightWrist.x - rightElbow.x
+            )) * 180 / Math.PI;
+
+            const avgArmAngle = (leftArmAngle + rightArmAngle) / 2;
+
+            // Shoulder press is considered "up" when arms are extended
+            const isPressUp = avgArmAngle > 150 && avgWristY < avgShoulderY - 20;
+            // Shoulder press is considered "down" when arms are bent
+            const isPressDown = avgArmAngle < 90 && avgWristY > avgShoulderY;
+
+            // Initialize wasDown if not present
+            if (this.wasDown === undefined) this.wasDown = false;
+
+            // Only count when going from down to up
+            if (isPressUp && this.wasDown) {
+                if (window.incrementReps) window.incrementReps();
+                this.wasDown = false;
+            }
+            if (isPressDown) {
                 this.wasDown = true;
             }
         }
